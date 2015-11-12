c032636ca3d8eb3633a422fcb01f9e52bb4bfc39
Pablite5 <Pablite5@GAMING>
2015-11-12 11:28:40 +0100

    finalizado19

---
+++ src/bol9/pkg1/Bol91.java
@@ -0,0 +1,16 @@
+
+package bol9.pkg1;
+
+/**
+ *
+ * @author Pablite5
+ */
+public class Bol91 {
+
+   
+    public static void main(String[] args) {
+       
+        Comparar.compararNumero();
+    }
+    
+}
